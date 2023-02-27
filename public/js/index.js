ffde62886586d26aba90b3886060e44176113f95
dsuekamling <dsuekamling@gmail.com>
2023-02-27 11:13:52 -0500

    app update

---
+++ public/js/index.js
@@ -0,0 +1,50 @@
+const searchBtn = document.getElementById("searchBtn");
+console.log(searchBtn); // check if searchBtn is being correctly selected
+searchBtn.addEventListener("click", searchRecipes);
+
+async function searchRecipes() {
+  console.log("searchRecipes function called!");
+  const query = document.getElementById("query").value;
+  const cuisine = document.getElementById("cuisine").value;
+  const diet = document.getElementById("diet").value;
+  const intolerances = document.getElementById("intolerances").value;
+  const includeInstructions = document.getElementById("includeInstructions").checked;
+  const includeIngredients = document.getElementById("includeIngredients").checked;
+
+  let url = `/search?query=${query}&cuisine=${cuisine}&diet=${diet}&intolerances=${intolerances}&includeNutrition=true`;
+  if (includeInstructions) {
+    url += "&instructionsRequired=true";
+  }
+  if (includeIngredients) {
+    const ingredients = document.getElementById("ingredients");//.value;
+    url += `&includeIngredients=${ingredients}`;
+  }
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  const resultsDiv = document.getElementById("results");
+  resultsDiv.innerHTML = "";
+
+  if (data && data.results) {
+    data.results.forEach(recipe => {
+      const recipeDiv = document.createElement("div");
+      recipeDiv.innerHTML = `
+        <h3>${recipe.title}</h3>
+        <p>${recipe.summary}</p>
+        <img src="${recipe.image}" alt="${recipe.title}">
+      `;
+      if (includeIngredients) {
+        recipeDiv.innerHTML += `<h4>Ingredients:</h4>
+          <p>${recipe.extendedIngredients.length > 0 ? recipe.extendedIngredients.map(ingredient => ingredient.original).join("<br>") : "No ingredients available."}</p>`;
+      }
+      if (includeInstructions) {
+        recipeDiv.innerHTML += `<h4>Instructions:</h4>
+          <p>${recipe.analyzedInstructions.length > 0 ? recipe.analyzedInstructions[0].steps.map(step => step.step).join("<br>") : "No instructions available."}</p>`;
+      }
+      resultsDiv.appendChild(recipeDiv);
+    });
+  } else {
+    resultsDiv.innerHTML = "No results found.";
+  }
+}
--- src/app.js
+++ src/app.js
@@ -1,13 +1,44 @@
 const path = require('path');
 const express = require('express');
 const hbs = require('hbs');
+const http = require('http');
+const fs = require('fs');
+const searchRouter = require('./utils/search');
 
 const app = express();
+
+
+app.use('/search', searchRouter);
 
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
 //setting up path for partials
 const partialsPath = path.join(__dirname, '../templates/partials')
+
+http.createServer(function (req, res) {
+    if (req.url === '/public/css/style.css') {
+      fs.readFile(__dirname + '/public/css/style.css', function (err, data) {
+        if (err) {
+          res.writeHead(404);
+          res.end(JSON.stringify(err));
+          return;
+        }
+        res.writeHead(200, {
+          'Content-Type': 'text/css'
+        });
+        res.write(data);
+        res.end();
+      });
+    }
+  });
+
+  app.use(express.static(path.join(__dirname, 'public'), { 
+    setHeaders: (res, path, stat) => {
+      if (path.endsWith('.css')) {
+        res.setHeader('Content-Type', 'text/css');
+      }
+    }
+  }));
 
 
 app.set('view engine', 'hbs');
@@ -15,6 +46,27 @@ app.set('views', viewsPath);
 hbs.registerPartials(partialsPath);
 
 app.use(express.static(publicDirectoryPath))
+
+// app.get("/search", async (req, res) => {
+//     const { default: fetch } = await import('node-fetch');
+  
+//     const query = req.query.query || "";
+//     const cuisine = req.query.cuisine || "";
+//     const diet = req.query.diet || "";
+//     const intolerances = req.query.intolerances || "";
+  
+//     const url = `https://api.spoonacular.com/recipes/complexSearch?apiKey=33f44c685ed146238f151c263016b42f&query=${query}&cuisine=${cuisine}&diet=${diet}&intolerances=${intolerances}&addRecipeInformation=true&fillIngredients=true&instructionsRequired=true`;
+  
+//     try {
+//       const response = await fetch(url);
+//       const data = await response.json();
+//       res.json(data);
+//     } catch (error) {
+//       console.log(error);
+//       res.json({ error: "An error occurred." });
+//     }
+  
+// });
 
 app.get('', (req, res) => {
     res.render('index', {
